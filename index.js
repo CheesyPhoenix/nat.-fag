2360d469416264c1273e53b9114cf1e12ab1b954
CheesyPhoenix <sindre.he.moe@gmail.com>
2021-12-03 15:10:23 +0100

    links - dark mode color

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ function toggle() {
 		state = "light";
 		document.getElementById("body").style =
 			"background-color: #dcdcdc; color: #292929;";
+		colorLinks("#0000ee");
 	} else if (state == "light") {
 		state = "dark";
 		document.getElementById("body").style =
 			"background-color: #292929; color: #dcdcdc;";
+		colorLinks("#8db2e5");
+	}
+}
+
+function colorLinks(hex) {
+	var links = document.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		if (links[i].href) {
+			links[i].style.color = hex;
+		}
 	}
 }
